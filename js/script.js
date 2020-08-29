74669facdf470d4f1666b0345acee63f30d74c68
nestor.junior <nestor.junior@sabion.com.br>
2020-08-29 17:48:04 -0300

    Include function toogle

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-const checkbox = document.getElementById('checkbox');
-const body = document.getElementsByTagName('body')[0];
 
-checkbox.addEventListener('change', () => {
-    body.classList.toggle('dark'); 
-})
+var toogleButton = function() {
+    const checkbox = document.getElementById('checkbox');
+    const body = document.getElementsByTagName('body')[0];
+
+    checkbox.addEventListener('change', () => {
+        body.classList.toggle('dark'); 
+    })
+
+};
+
+toogleButton();
